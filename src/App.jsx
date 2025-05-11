f2706ecd4dcfd5050d7f30d97f0b39e49b950391
Sahil Bhande <sahilbhande1234@gmail.com>
2025-05-11 20:49:23 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar"
+import Home from "./components/Home"
+import About from "./components/About"
+import Skills from "./components/Skills"
+import Work from "./components/work"
+import Contact from "./components/contact"
+
+function App() {
+  return (
+    <div>
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Skills/>
+    <Work/>
+    <Contact/>
+    </div>
+  )
+}
+
+export default App
